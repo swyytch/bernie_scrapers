e44741a74ec0ba94b49332e5ec10c1eccc6fa22f
Russell Troxel <russell.troxel@rackspace.com>
2015-10-27 16:59:42 -0500

    bugfix scraper config

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -16,23 +16,25 @@ class Scraper(object):
     __metaclass__ = ABCMeta
 
     def __init__(self):
+        self.configfile = "/opt/bernie/config.yml"
+        self.config = self.get_config()
         self.db = self.mongo().bernie
 
     def sanitize_soup(self, soup):
-      # inspired by https://chase-seibert.github.io/blog/2011/01/28/sanitize-html-with-beautiful-soup.html  
+        # inspired by https://chase-seibert.github.io/blog/2011/01/28/sanitize-html-with-beautiful-soup.html  
 
-      blacklist = ["script", "noscript", "video"]
+        blacklist = ["script", "noscript", "video"]
 
-      for tag in soup.findAll():
-        if tag.name.lower() in blacklist:
-            tag.extract()
+        for tag in soup.findAll():
+            if tag.name.lower() in blacklist:
+                tag.extract()
 
-      comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-      for comment in comments:
-          comment.extract()
+        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+        for comment in comments:
+            comment.extract()
 
-      return soup
-    
+        return soup
+
     def replace_with_newlines(self, element):
         text = ''
         for elem in element.recursiveChildGenerator():
@@ -42,7 +44,7 @@ class Scraper(object):
                 text += '\n'
         return text
 
-    def config(self):
+    def get_config(self):
         try:
             with open(self.configfile, 'r') as f:
                 conf = yaml.load(f)
