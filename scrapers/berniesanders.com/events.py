436550ef0fc73c2ec56c53f9b99c622a91ac9e7a
Russell Troxel <russell.troxel@rackspace.com>
2015-11-30 19:20:09 -0600

    removed a few dependencies, cleaned-up, validated to pep-8

--- scrapers/berniesanders.com/events.py
+++ scrapers/berniesanders.com/events.py
@@ -2,13 +2,14 @@
 
 import logging
 
+import hashlib
+import hmac
 import time
-import calendar
-import hmac, hashlib
 import urllib
 
 from datetime import datetime
 from dateutil import parser
+
 from HTMLParser import HTMLParser
 
 if __name__ == "__main__":
@@ -48,45 +49,46 @@ class EventScraper(Scraper):
     def __init__(self):
         Scraper.__init__(self)
         c = self.config["bsd"]
-        endpoint = c["endpoint"]
-        api_id = c["api_id"]
-        api_secret = c["api_secret"]
-        self.url = self.generate_bsd_url(endpoint, "/page/api/event/search_events", api_id, api_secret)
         self.html = HTMLParser()
+        self.call_path = "/page/api/event/search_events"
         self.params = {
-            'orderby': 'zip_radius',
-            'zip_radius[1]': '6000',
-            'zip_radius[0]': '78218',
-            'radius_unit': 'mi',
-            'country': 'US',
-            'format': 'json'
+            "api_ver": "2",
+            "api_id": c["api_id"],
+            "api_ts": str(int(time.time()))
         }
+        self.signed_params = self.sign_params(c["api_secret"])
+        self.url = "".join([
+            c["endpoint"],
+            self.call_path,
+            "?",
+            self.signed_params
+        ])
         self.map = {
             "event_id": "original_id",
             "start_dt": "start_time"
         }
 
-    def generate_bsd_url(self, endpoint, call_path, api_id, api_secret, params={}):
-      timestamp = str(calendar.timegm(time.gmtime()))
-      params["api_ver"] = "2"
-      params["api_id"] = api_id
-      params["api_ts"] = timestamp
-   
-      unencoded_query_string = "&".join([key + "=" + str(params[key]) for key in params])
-      signing_string = "\n".join([
-        params["api_id"],
-        params["api_ts"],
-        call_path,
-        unencoded_query_string
-      ])
-      
-      api_mac = hmac.new(api_secret.encode(), signing_string.encode(), hashlib.sha1).hexdigest()
-      params["api_mac"] = api_mac
-      
-      unencoded_query_string = "&".join([key + "=" + str(params[key]) for key in params])
-      encoded_query_string = urllib.quote(unencoded_query_string, safe="=&")
-      
-      return endpoint + call_path + "?" + encoded_query_string
+    def sign_params(self, api_secret):
+
+        signing_string = "\n".join([
+            self.params["api_id"],
+            self.params["api_ts"],
+            self.call_path,
+            urllib.urlencode(self.params, doseq=True)
+        ])
+
+        api_mac = hmac.new(
+            api_secret.encode(),
+            signing_string.encode(),
+            hashlib.sha1
+        ).hexdigest()
+        self.params["api_mac"] = api_mac
+
+        query_string = "&".join(
+            [key + "=" + str(self.params[key])
+             for key in self.params]
+        )
+        return query_string
 
     def translate(self, result):
         # Translate normal key names based on map
@@ -120,13 +122,14 @@ class EventScraper(Scraper):
         result["lang"] = "en"
         # convert capacity and attendee_count to int's
         for x in ["capacity", "attendee_count"]:
-            if x in result and result[x] != None:
+            if x in result and result[x] is not None:
                 result[x] = int(result[x])
 
         # Convert str to datetime
         result["start_time"] = parser.parse(result["start_time"])
         result["event_date"] = str(result["start_time"].date())
         result["is_official"] = result["is_official"] == "1"
+
         # remove any unneeded keys
         keys = result.keys()
         for k in keys:
@@ -136,7 +139,7 @@ class EventScraper(Scraper):
 
     def go(self):
         r = self.get(
-            self.url,
+            "".join(self.url),
             result_format="json"
         )
         for result in r:
@@ -154,7 +157,6 @@ class EventScraper(Scraper):
                 logging.info(msg.format(rec["name"]))
                 rec["inserted_at"] = datetime.now()
                 self.db.events.insert_one(rec)
-
 if __name__ == "__main__":
     bernie = EventScraper()
     bernie.go()
